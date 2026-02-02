71c08f1cbc12581f8144384c908e2f35fdc80f34
eottabom <snoopy12oyk@gmail.com>
2026-02-02 22:19:15 +0900

    feat: api-backend init

---
+++ perf-backend/src/main/java/eottabom/perf/PerfBackendApplication.java
@@ -0,0 +1,12 @@
+package eottabom.perf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfBackendApplication.class, args);
+    }
+}
